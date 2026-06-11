fb5986d537f17c16dcb565e606c9016179c845f6
RoshanGurung-Eng <roshantheaomine@gmail.com>
2026-06-11 09:31:36 +0545

    test

--- orders/tests.py
+++ orders/tests.py
@@ -1,9 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-print("Testing django")
-print("Nothing")
-print("Test")
-print("trying Bluetooth Keyboards")
-print("still testing")
-print("Still Learnig and Testing")
